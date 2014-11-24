6e187d4f4b7ff234b1002f3b498e9c4ff96da7f8
Takeo Tamura <takeo@cubdesign.com>
2014-11-24 17:32:39 +0900

    jekyllからmiddlemanに変更

--- script.js
+++
@@ -1,8 +0,0 @@
-function popup(URLStr,Name, width, height)
-{
-	var popUpWin;
-	popUpWin=window.open(URLStr, Name, 'toolbar=no,location=no,directories=no,status=no,menub ar=no,scrollbars=no,resizable=no,copyhistory=no,width='+width+',height='+height+'');
-	popUpWin.window.focus();
-	popUpWin.window.moveTo(670, 115) 
-}
-
